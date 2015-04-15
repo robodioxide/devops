8bf24f0a8cbd868d61c9ad02a4f166b513dff734
robodioxide <nah>
2015-04-14 22:43:31 -0500

    Learning some Rubey\!

---
+++ howto/ruby/testscript-01.rb
@@ -0,0 +1,16 @@
+## Just finished my first lesson for Ruby on CodeAcademy!
+puts "Hello World, this is calculating 16/9 as"
+print "  "
+puts 16 / 9
+print "but that is a poor estimation of 1.618"
+print
+
+variable_string = "I guess I have a string a ding."
+variable_numero_uno = 867.5309
+variable_boolean = false
+
+print variable_numero_uno
+print " is good for the soul, and apparently "
+puts variable_string
+
+print variable_boolean
